ff3e5f42136164e4d8ba6b312c5a702e5a07289e
33741765 <1448834706@qq.com>
2021-12-09 17:54:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-jkntwg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-jkntwg)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+import {} from 'leaflet';
+
+// Write Javascript code!
+const appDiv = document.getElementById('map');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
