b013035458aacdcd15f605cc753031e27687c987
33741765 <1448834706@qq.com>
2021-12-10 10:52:46 +0800

    3 files modified

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // Import stylesheets
 import './style.css';
-import {} from 'leaflet';
+import { Map } from 'leaflet';
 
 // Write Javascript code!
-const appDiv = document.getElementById('map');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// 创建高德Map
+const amap = new AMap.Map('amap', {
+  fadeOnZoom: false,
+  navigationMode: 'classic',
+  optimizePanAnimation: false,
+  animateEnable: false,
+  dragEnable: false,
+  zoomEnable: false,
+  resizeEnable: true,
+  doubleClickZoom: false,
+  keyboardEnable: false,
+  scrollWheel: false,
+  expandZoomRange: true,
+  zooms: [1, 20],
+  mapStyle: 'normal',
+  features: ['road', 'point', 'bg'],
+  viewMode: '2D',
+});
+// 创建leaflet Map
+const map = new Map('map');
